f3824ee2349a058a43aba65cc5a5a94ce7762260
triestpa <patrick.triest@gmail.com>
2017-08-13 17:14:07 +0700

    Add distribution test

---
+++ test/distributionWorker.js
@@ -0,0 +1,44 @@
+importScripts('../prng.min.js')
+
+// Our PRNG returns values between 0 and 1000000
+const max = 10 ** 6
+
+function getPsuedoRandomValue () {
+  // Initialize the PRNG with a random seed each time.
+  let seed = Math.floor(Math.random() * (10 ** 12))
+  const generator = new PRNG(seed)
+  return generator.nextBoundedInt(0, max)
+}
+
+// We'll be charting the results in 100 bins on the UI thread
+const ticks = 100
+
+// The incremental size of each bin
+const binSize = max / ticks
+
+// Send data to UI thread after every 5,000,000 numbers
+const postInterval = 5 * (10 ** 6)
+
+// Initialize empty bin dictionary
+const bins = {}
+
+// Fill the bins with zeros
+for (let i = 0; i < ticks; i++) {
+  bins[i * binSize] = 0
+}
+
+// Round the number down to the closest bin
+function roundToBin(number) {
+  return Math.floor(number / binSize) * binSize
+}
+
+// Generate up to 1 billion random numbers
+for (let i = 1; i <= 10 ** 9; i++) {
+  // Increment the bin that the number falls into
+  bins[roundToBin(getPsuedoRandomValue())] += 1
+
+  // Once the post interval is reached, post data to the UI thread
+  if (i % postInterval === 0) {
+    postMessage(bins)
+  }
+}
